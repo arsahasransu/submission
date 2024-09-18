138e1a9b167212590831a6e78ea399f6a108a233
Gianluca <gianluca.cerminara@cern.ch>
2024-09-18 10:54:39 +0200

    fix untracked

--- templates/jobCustomization_INFP_cfg.py
+++ templates/jobCustomization_INFP_cfg.py
@@ -4,9 +4,9 @@ from input_cfg import process
 
 process.maxEvents.input = cms.untracked.int32(TEMPL_NEVENTS)
 process.source.fileNames = cms.untracked.vstring(TEMPL_INFILES)
-process.out.fileName = cms.string('TEMPL_OUTFILE')
 
 #Setup FWK for multithreaded
 process.options.numberOfThreads=cms.untracked.uint32(TEMPL_NCPU)
 process.options.numberOfStreams=cms.untracked.uint32(0)
 process.options.numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(1)
+process.out.fileName = cms.untracked.string('TEMPL_OUTFILE')
